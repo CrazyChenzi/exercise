945e1284f18aba0a1cbbfb6a3755aa815bd5f209
yjc <756319278@qq.com>
2018-12-27 16:54:48 +0800

    update ts-demo

---
+++ vue/ts-vue-demo/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld :msg="msg"/>
+  </div>
+</template>
+
+<script lang="ts">
+
+
+
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+interface Shape {
+    color: string;
+}
+
+interface PenStroke {
+    penWidth: number;
+}
+
+interface Square extends Shape, PenStroke {
+    sideLength: number;
+}
+
+let square = <Square>{};
+square.color = "blue";
+square.sideLength = 10;
+square.penWidth = 5.0;
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+  public data() {
+    return {
+      msg: 'welcome ts',
+    };
+  }
+}
+</script>
